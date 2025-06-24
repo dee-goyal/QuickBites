e8b192554a6fd2caffc64f8c1937ba4ab963ac3b
Deepika Goyal <mco22385@ccet.ac.in>
2025-06-24 12:25:32 +0530

    index.js fixed

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoDB = require("./db");
 
-console.log("Global items:", global.items);
-console.log("Global categories:", global.category);
+console.log("Starting the application...");
+mongoDB();
 
-app.use(function (req, res, next) {
+app.use(express.json());
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -15,18 +17,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-console.log("Starting the application...");
-mongoDB();
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.use(express.json());
 app.use("/api", require("./routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
-app.use("/api", require("./Routes/OrderData"));
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
